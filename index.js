dceaa36d050a72c5ae67be4c5dc8a1fc98951253
Anouwong <51252090+Anouwong@users.noreply.github.com>
2020-08-02 12:28:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  let lang = require(`./languages/${language}/command.json`); //ตัวดึงภาษา
+  let lang;
+  if(language == "th-TH"){
+    lang = require(`command_th.json`); //ตัวดึงภาษาไทย
+  } else {
+    lang = require(`command_en.json`); //ตัวดึงภาษาอังกฤษ
+  }
   
   if (msg.content === 'ping') {
     msg.reply(lang.pong); //ตัวที่ดึงมาในส่วน pong ในไฟล์ command.json
